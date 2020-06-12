b24bee321b84ca33299949704181c77032c515d8
Alejandro Gomez <m.alejandro.gomez.v@gmail.com>
2020-06-11 22:39:25 -0500

    Model and controller
    Model an controller TablaViewModel

---
+++ MVCCrud/MVCCrud/Controllers/TablaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Controllers;
+using MVCCrud.Models;
+using MVCCrud.Models.ViewModels;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (CrudEntities db = new CrudEntities())
+            {
+                lst = (from d in db.Tabla
+                       select new ListTablaViewModel
+                       {
+                           Id = d.id,
+                           Nombre = d.correo,
+                           Correo = d.correo
+                       }).ToList();
+            }
+            return View(lst);
+        }
+    }
+}
